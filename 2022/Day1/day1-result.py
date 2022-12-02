bf19ba77f7746abb7073a83b5b8d2233dd6c99ad
laitiesa <119692029+laitiesa@users.noreply.github.com>
2022-12-02 19:01:56 +0200

    Add day 1 and day 2

---
+++ 2022/Day1/day1-result.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 14:29:27 2022
+
+@author: Esa
+"""
+
+""" AOC 2022 Day 1
+"""
+import os
+user = os.path.expanduser('~')
+
+test_input = user + "/OneDrive/Tiedostot/AoC2022/Day1/test-input-1.txt"
+real_input = user + "/OneDrive/Tiedostot/AoC2022/Day1/input-1.txt"
+"""
+Part 1
+"""
+max_sum = 0
+temp_sum = 0
+
+
+test = False
+
+if test:
+    with open(test_input) as f:
+        for i in f:
+            if i == '\n' or i == '':
+                if temp_sum > max_sum:
+                    max_sum = temp_sum
+                temp_sum = 0
+            else:
+                    temp_sum = temp_sum + int(i)
+                    #print(int(i))
+    if temp_sum > max_sum:
+        max_sum = temp_sum
+    print("Part 1 test result is: " + str(max_sum))
+
+if not test:
+    with open(real_input) as f:
+        for i in f:
+            if i == '\n' or i == '':
+                if temp_sum > max_sum:
+                    max_sum = temp_sum
+                temp_sum = 0
+            else:
+                temp_sum = temp_sum + int(i)
+                #print(int(i))
+    if temp_sum > max_sum:
+        max_sum = temp_sum
+    print("Part 1  real input result is: " + str(max_sum))
+    
+    
+"""
+Part 2
+"""
+list1 = [0, 0, 0]
+temp_sum = 0
+
+
+test = False
+
+if test:
+    with open(test_input) as f:
+        for i in f:
+            if i == '\n' or i == '':
+                #print(temp_sum)
+                for j in list1:
+                    if temp_sum > j:
+                        list1.pop(0)
+                        list1.append(temp_sum)
+                        list1.sort()
+                        break
+                temp_sum = 0
+            else:
+                    temp_sum = temp_sum + int(i)
+                    #print(int(i))
+                    #print(temp_sum)
+    for j in list1:
+        if temp_sum > j:
+            list1.pop(0)
+            list1.append(temp_sum)
+            list1.sort()
+            break
+    print("Part 2 test result list is: " + str(list1) + " and sum is: " + str(sum(list1)))
+
+if not test:
+    with open(real_input) as f:
+        for i in f:
+            if i == '\n' or i == '':
+                #print(temp_sum)
+                for j in list1:
+                    if temp_sum > j:
+                        list1.pop(0)
+                        list1.append(temp_sum)
+                        list1.sort()
+                        break
+                temp_sum = 0
+            else:
+                    temp_sum = temp_sum + int(i)
+                    #print(int(i))
+                    #print(temp_sum)
+    for j in list1:
+        if temp_sum > j:
+            list1.pop(0)
+            list1.append(temp_sum)
+            list1.sort()
+            break
+    print("Part 2 real result list is: " + str(list1) + " and sum is: " + str(sum(list1)))
---
+++ 2022/Day2/day2-result.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  2 12:36:16 2022
+
+@author: Esa
+"""
+
+""" AOC 2022 Day 2
+"""
+import os
+user = os.path.expanduser('~')
+AoCDir = "/OneDrive/Tiedostot/AoC2022/Day"
+Day = 2
+
+test_input =  user + AoCDir + str(Day) + "/test-input-" + str(Day) + ".txt"
+real_input =  user + AoCDir + str(Day) + "/input-" + str(Day) + ".txt"
+
+
+"""
+Part 1
+
+A = Rock
+B = Paper
+C = Scissors
+
+X = Rock
+Y = Paper
+Z = Scissors
+
+"""
+
+selectedInput = None
+
+Opponent = None
+Self = None
+first = None
+second = None
+
+shapeScore = 0
+runningScore = 0
+finalScore = 0
+roundScore = 0
+
+test = False
+
+if test:
+    selectedInput = test_input
+else:
+    selectedInput = real_input
+    
+with open(selectedInput) as f:
+        for i in f:
+            x = i.rstrip().split(" ")
+            #print(x)
+            roundScore = 0
+            first = ord(x[0])-64
+            second = ord(x[1])-87
+            roundScore = second
+            result = first - second
+            if result == 0:
+                roundScore = roundScore + 3
+            elif result == -1 or result == 2:
+                roundScore = roundScore + 6
+            elif result < -1:
+                roundScore = roundScore + 0
+            #print(roundScore)
+            runningScore = runningScore + roundScore
+finalScore = runningScore
+#print(shapeScore)
+#print(winScore)
+print("part1 " + str(finalScore))
+                
+# else:
+#     with open(real_input) as f:
+#         for i in f:
+#             x = i.rstrip().split(" ")
+#             #print(x)
+#             roundScore = 0
+#             first = ord(x[0])-64
+#             second = ord(x[1])-87
+#             roundScore = second
+#             result = first - second
+#             if result == 0:
+#                 roundScore = roundScore + 3
+#             elif result == -1 or result == 2:
+#                 roundScore = roundScore + 6
+#             elif result < -1:
+#                 roundScore = roundScore + 0
+#             #print(roundScore)
+#             runningScore = runningScore + roundScore 
+#     finalScore = runningScore
+#     #print(shapeScore)
+#     #print(winScore)
+#     print("part1 " + str(finalScore))
+
+
+"""
+Part 2
+
+A = Rock
+B = Paper
+C = Scissors
+
+X = Lose
+Y = Draw
+Z = Win
+
+"""
+Opponent = None
+Self = None
+first = None
+second = None
+
+shapeScore = 0
+runningScore = 0
+finalScore = 0
+roundScore = 0
+
+if test:
+    selectedInput = test_input
+else:
+    selectedInput = real_input
+
+with open(selectedInput) as f:
+        for i in f:
+            x = i.rstrip().split(" ")
+            #print(x)
+            roundScore = 0
+            first = ord(x[0])-64
+            second = ord(x[1])-87
+            #roundScore = second
+            result = second
+            if result == 2:
+                roundScore = roundScore + 3 + first
+            elif result == 3:
+                roundScore = roundScore + 6
+                if first == 1:
+                    roundScore = roundScore + 2
+                elif first == 2:
+                    roundScore = roundScore + 3
+                elif first == 3:
+                    roundScore = roundScore + 1    
+            elif result == 1:
+                roundScore = roundScore + 0
+                if first == 1:
+                    roundScore = roundScore + 3
+                elif first == 2:
+                    roundScore = roundScore + 1
+                elif first == 3:
+                    roundScore = roundScore + 2   
+            #print(roundScore)
+            runningScore = runningScore + roundScore
+finalScore = runningScore
+#print(shapeScore)
+#print(winScore)
+print("part2 " + str(finalScore))
+                
